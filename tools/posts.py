4d95a082b704ca0bb90803d577088450d475a3c4
OhYee <oyohyee@oyohyee.com>
2020-01-29 17:29:19 +0800

    misc: re-render markdown in post(#master)

--- tools/posts.py
+++ tools/posts.py
@@ -1,5 +1,6 @@
 import utils
 import time
+import requests
 
 document = utils.mydb["tags"]
 tags = {tag["name"]: tag["_id"] for tag in document.find({})}
@@ -11,10 +12,10 @@ new_data = list(map(lambda x: {
     "title": x["title"],
     "abstract": x["abstruct"],
     "view": int(x["view"]),
-    "url": x["url"].lower(),
+    "url": x["url"].lower().replace("/", "_"),
     "publish_time": int(time.mktime(time.strptime(x["time"], "%Y-%m-%d %H:%M:%S"))),
     "edit_time": int(time.mktime(time.strptime(x["updatetime"], "%Y-%m-%d %H:%M:%S"))),
-    "content": x["html"],
+    "content":  requests.post("http://127.0.0.1:50000/api/markdown", {"source": x["raw"]}).json()["html"],
     "raw": x["raw"],
     "tags": [tags[tag] for tag in x["tags"].split(",")],
     "keywords": [],
