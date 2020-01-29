093085778b1f9db868e8b787548e761de329f466
OhYee <oyohyee@oyohyee.com>
2020-01-29 17:28:58 +0800

    misc: re-render markdown in comment (#master)

--- tools/comments.py
+++ tools/comments.py
@@ -1,6 +1,7 @@
 import utils
 import time
 import re
+import requests
 
 old_data = utils.query("select * from comments;")
 
@@ -12,11 +13,11 @@ new_data = list(map(lambda x: {
     "avatar": "",
     "time": int(time.mktime(time.strptime(x["time"], "%Y-%m-%d %H:%M:%S"))),
     "raw": re.sub(r'@[0-9]+#', "", x["raw"]),
-    "content": x["html"],
-    "url": x["url"].lower(),
+    "content": requests.post("http://127.0.0.1:50000/api/markdown", {"source": re.sub(r'@[0-9]+#', "", x["raw"])}).json()["html"],
+    "url": "/"+("post/" + x["url"][5:].replace("/", "_") if x["url"][0:5] == "post/" else x["url"]).lower(),
     "show": x["show"] == "true",
     "ad": x["ad"] == "true",
-    "reply": int(temp[0]) if (temp: = re.findall(r'@([0-9]+)#', x["raw"])) and len(temp) > 0 else -1,
+    "reply": int(temp[0]) if (temp:= re.findall(r'@([0-9]+)#', x["raw"])) and len(temp) > 0 else -1,
 }, old_data))
 
 for d in new_data:
