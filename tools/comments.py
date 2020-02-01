43282828ce9350d342c47e6594d9cd1fd7cd7202
OhYee <oyohyee@oyohyee.com>
2020-02-01 23:42:15 +0800

    misc: comment struct (#master)

--- tools/comments.py
+++ tools/comments.py
@@ -2,27 +2,49 @@ import utils
 import time
 import re
 import requests
+import bson
 
-old_data = utils.query("select * from comments;")
+data = sorted(utils.query("select * from comments;"),
+              key=lambda x: int(x["id"]))
 
 
-new_data = list(map(lambda x: {
-    "id": int(x["id"]),
-    "email": x["email"],
-    "recv": x["sendemail"] == "true",
-    "avatar": "",
-    "time": int(time.mktime(time.strptime(x["time"], "%Y-%m-%d %H:%M:%S"))),
-    "raw": re.sub(r'@[0-9]+#', "", x["raw"]),
-    "content": requests.post("http://127.0.0.1:50000/api/markdown", {"source": re.sub(r'@[0-9]+#', "", x["raw"])}).json()["html"],
-    "url": "/"+("post/" + x["url"][5:].replace("/", "_") if x["url"][0:5] == "post/" else x["url"]).lower(),
-    "show": x["show"] == "true",
-    "ad": x["ad"] == "true",
-    "reply": int(temp[0]) if (temp:= re.findall(r'@([0-9]+)#', x["raw"])) and len(temp) > 0 else -1,
-}, old_data))
+m = {}
 
-for d in new_data:
-    print(d)
+defaultObjectID = bson.ObjectId("000000000000000000000000")
+
+
+def transfer(x):
+    temp = re.findall(r'@([0-9]+)#', x["raw"])
+    url = ""
+    if x["url"][0:5] == "post/":
+        url = "/post/" + x["url"][5:].replace("/", "_")
+    elif x["url"] == "pages/comments":
+        url = "/comment"
+    else:
+        url = "/"+x["url"]
+    url = url.lower()
+
+    return {
+        # "id": int(x["id"]),
+        "email": x["email"],
+        "recv": x["sendemail"] == "true",
+        "avatar": requests.get("http://127.0.0.1:50000/api/avatar", {"email": x["email"]}).json()["avatar"],
+        "time": int(time.mktime(time.strptime(x["time"], "%Y-%m-%d %H:%M:%S"))),
+        "raw": re.sub(r'@[0-9]+#', "", x["raw"]),
+        "content": requests.post("http://127.0.0.1:50000/api/markdown", {"source": re.sub(r'@[0-9]+#', "", x["raw"])}).json()["html"],
+        "url": url,
+        "show": x["show"] == "true",
+        "ad": x["ad"] == "true",
+        "reply": m.get(int(temp[0]), defaultObjectID) if len(temp) > 0 else defaultObjectID,
+    }
+
 
 document = utils.mydb["comments"]
 document.delete_many({})
-ids = document.insert_many(new_data)
+
+
+for i, d in enumerate(data):
+    print(i, d)
+    objId = document.insert_one(transfer(d))
+    m[i+1] = objId.inserted_id
+    print(type(objId.inserted_id))
