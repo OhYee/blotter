82bfc8b889b86a3e4c9c45179a46367a56cb5fd8
OhYee <oyohyee@oyohyee.com>
2020-02-12 17:41:10 +0800

    fix: layout api will only call in server, no need to return token(#master)

--- api/layout.go
+++ api/layout.go
@@ -15,7 +15,7 @@ type LayoutResponse struct {
 	View     int         `json:"view"`
 	Beian    string      `json:"beian"`
 	BlogName string      `json:"blog_name"`
-	Token    string      `json:"token"`
+	// Token    string      `json:"token"`
 }
 
 // Layout get site base info
@@ -39,7 +39,7 @@ func Layout(context *register.HandleContext) (err error) {
 	if err = m.SetString("blog_name", &res.BlogName); err != nil {
 		return
 	}
-	res.Token = context.GetCookie("token")
+	// res.Token = context.GetCookie("token")
 
 	go func() {
 		mongo.Update(
