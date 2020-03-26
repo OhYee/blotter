175db6a8630028130123016de4d5943a08b06f8a
OhYee <oyohyee@oyohyee.com>
2020-03-26 16:05:30 +0800

    misc: spider for tinshine.cn(#master)

---
+++ spider/tinshine.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+from utils import Site, Post, get
+
+
+class Tinshine(Site):
+    def __init__(self):
+        super(Site, self)
+
+    def matcher(self, url: str):
+        return 'www.tinshine.cn' in url
+
+    def solver(self, url: str):
+        res = get(
+            "%s/%s" %
+            (url.strip("/"), "front/articles.action?pageCount=1")
+        )
+        soup = BeautifulSoup(res, features="lxml")
+        posts = []
+        for item in soup.select(".card"):
+            posts.append(Post(
+                item.select_one(".card-body").get_text(),
+                "%s/%s" % (url.strip("/"),
+                           item.parent.get("href").strip("/"))
+            ))
+        return posts
+
+
+if __name__ == '__main__':
+    t = Tinshine()
+    print(t.matcher("http://www.tinshine.cn/"))
+    print(t.solver("http://www.tinshine.cn/"))
