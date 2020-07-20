c285b99c84dceaa8fbaf3a009908e98c14f5b43d
OhYee <oyohyee@oyohyee.com>
2020-07-21 02:24:00 +0800

    fix: WithCount with description field(#master)

--- api/pkg/tag/type.go
+++ api/pkg/tag/type.go
@@ -1,21 +1,17 @@
 package tag
 
-type Base struct {
-	ID    string `json:"id" bson:"_id"`
-	Name  string `json:"name" bson:"name"`
-	Short string `json:"short" bson:"short"`
-	Icon  string `json:"icon" bson:"icon"`
-	Color string `json:"color" bson:"color"`
-}
-
 // Type type
 type Type struct {
-	Base        `bson:",inline"`
+	ID          string `json:"id" bson:"_id"`
+	Name        string `json:"name" bson:"name"`
+	Short       string `json:"short" bson:"short"`
+	Icon        string `json:"icon" bson:"icon"`
+	Color       string `json:"color" bson:"color"`
 	Description string `json:"description" bson:"description"`
 }
 
 // WithCount tag type with count
 type WithCount struct {
-	Base  `bson:",inline"`
+	Type  `bson:",inline"`
 	Count int64 `json:"count" bson:"count"`
 }
