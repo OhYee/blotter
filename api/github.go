3605e07a087e4f512c2d81e0eea7ba095e932666
OhYee <oyohyee@oyohyee.com>
2020-10-10 22:56:40 +0800

    misc: use vmess if has vmess variable(#master)

--- api/github.go
+++ api/github.go
@@ -5,13 +5,37 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 
+	"github.com/OhYee/blotter/api/pkg/variable"
 	"github.com/OhYee/blotter/register"
 	"github.com/OhYee/rainbow/errors"
+	mv2ray "github.com/v2fly/vmessping/miniv2ray"
 )
 
 func githubRepo(username string, page int) (res []map[string]interface{}, err error) {
-	resp, err := http.Get(fmt.Sprintf("https://api.github.com/users/%s/repos?page=%d", username, page))
+	var client *http.Client = nil
+
+	vars, err := variable.Get("vmess")
+	vmessLink, _ := vars.GetString("vmess")
+	fmt.Println(vmessLink)
+	if vmessLink != "" {
+		server, err := mv2ray.StartV2Ray(vmessLink, false, true)
+		if err == nil {
+			err = server.Start()
+			if err == nil {
+				defer server.Close()
+				client, _ = mv2ray.CoreHTTPClient(server, time.Duration(10*time.Second))
+			}
+		}
+	}
+
+	if client == nil {
+		fmt.Println("use default")
+		client = http.DefaultClient
+	}
+
+	resp, err := client.Get(fmt.Sprintf("https://api.github.com/users/%s/repos?page=%d", username, page))
 	if err != nil {
 		return
 	}
