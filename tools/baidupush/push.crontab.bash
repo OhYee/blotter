81fb0d8374c0d9192d2f950e0d800739c2c71917
OhYee <oyohyee@oyohyee.com>
2020-10-26 15:14:31 +0800

    fix: baidu push script(#master)

--- tools/baidupush/push.crontab.bash
+++ tools/baidupush/push.crontab.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 
-curl https://www.oyohyee.com/sitemap.txt > $SHELL_FOLDER/urls.txt 1>>$SHELL_FOLDER/log.log 2>>$SHELL_FOLDER/error.log
+curl https://www.oyohyee.com/sitemap.txt 1>$SHELL_FOLDER/urls.txt 2>>$SHELL_FOLDER/error.log
 curl -H 'Content-Type:text/plain' --data-binary @$SHELL_FOLDER/urls.txt "http://data.zz.baidu.com/urls?site=www.oyohyee.com&token=Ah36uCNcnwq2q7LX" 1>>$SHELL_FOLDER/log.log 2>>$SHELL_FOLDER/error.log
