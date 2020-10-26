cf338a3c71d0c43c357f645d288623b7ba0d96b3
OhYee <oyohyee@oyohyee.com>
2020-10-26 15:04:23 +0800

    misc: add spider bash script(#master)

---
+++ tools/spider/spider.crontab.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# ROOT user need next line to set environment variables
+# export PYTHONPATH="/usr/lib/python38.zip:/usr/lib/python3.8:/usr/lib/python3.8/lib-dynload:/home/ubuntu/.local/lib/python3.8/site-packages:/usr/local/lib/python3.8/dist-packages:/usr/lib/python3/dist-packages"
+
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+
+python3 $SHELL_FOLDER/main.py 1>>$SHELL_FOLDER/log.log 2>>$SHELL_FOLDER/error.log%
