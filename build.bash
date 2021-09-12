2a2786a95d8c927faa27025d6b17c46f2ed603c2
OhYee <oyohyee@oyohyee.com>
2021-09-13 01:13:37 +0800

    workflow: test build

--- build.bash
+++ build.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 TAG=$(git describe --abbrev=0 --tags 2>/dev/null || git rev-parse --short HEAD)
+
+if [[ $1 == 'test' ]]; then
+    TAG="test"
+fi
+
 IMAGE="ohyee/blotter:${TAG}"
 echo $IMAGE
 docker build -t ${IMAGE} .
